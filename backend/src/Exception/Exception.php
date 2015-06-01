e736c2bed4b46dc845ebec9ec075448b6ba73c03
Cagatay Gurturk <cagatay@itusozluk.com>
2015-06-01 23:19:27 +0200

    Add Backend exceptions

---
+++ backend/src/Exception/Exception.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Copyright (C) 2015 cagatay
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Backend\Exception;
+
+/**
+ * Exception are used to give right HTTP Status Codes at the right time
+ * We normally create child class for exceptions and by default all of them will throw 500 HTTP status code.
+ *
+ * @author cagatay
+ */
+class Exception extends \Exception {
+
+    protected $code = 500;
+
+}
