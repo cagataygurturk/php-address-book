4071bd01ec18c848fce5bf41efe1b9faf7734e79
Cagatay Gurturk <cagatay@itusozluk.com>
2015-06-02 04:20:05 +0200

    Update test

--- framework/tests/FrameworkTest/MVC/ViewModel/ViewModelTest.php
+++ framework/tests/FrameworkTest/MVC/ViewModel/ViewModelTest.php
@@ -16,10 +16,14 @@ class RouterTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testXMLViewModel() {
+        
         $xmlViewModel = new \Framework\MVC\ViewModel\XMLViewModel(array('test' => array('test2')));
         $render = $xmlViewModel->render();
-        $this->assertContains('<results>', $render);
-        $this->assertContains('</results>', $render);
+        echo $render;
+        $this->assertContains('<root>', $render);
+        $this->assertContains('<test>', $render);
+        $this->assertContains('<test>', $render);
+        $this->assertContains('</root>', $render);
     }
 
 }
