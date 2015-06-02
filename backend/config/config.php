f78cb4a84307ebb4604cc40e1a31a376ad6d2e96
Cagatay Gurturk <cagatay@itusozluk.com>
2015-06-02 13:48:07 +0200

    Comments

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,7 +1,15 @@
 <?php
 
 return array(
-    'repositories' => array('person' => array(
+    'repositories' => array(
+        /*
+         * Each section behaves like RDBMS tables. Each table, thus, should have its own .csv file
+         * class indicates the Repository class for this table
+         * hydrate_to_object indicates instances of which class will be created for each line in the .csv file
+         * fields should include column names for comma-seperated values and they have to have their
+         * getter and setters in hydrate_to_object class
+         */
+        'person' => array(
             'class' => '\Backend\Infrastructure\Persistence\Repository\PersonRepository',
             'file' => __DIR__ . '/../../data/example.csv',
             'hydrate_to_object' => '\Backend\Model\Person',
